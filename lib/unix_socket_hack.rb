dcf92436d4a8524c93ef6148cf09b934e2a67e53
Keiji, Yoshimi <walf443@gmail.com>
2012-05-07 09:43:29 +0900

    initial commit.

---
+++ lib/unix_socket_hack.rb
@@ -0,0 +1,41 @@
+require 'socket'
+
+# support unixsocket to tcp only supproted library.
+# @example
+#   require 'unix_socket_hack'
+#   UNIXSocketHack.hack({ 'unixsocket:9999' => '/path/to/unix.sock' })
+#   sock = TCPSocket.new('unixsocket', 9999)
+#   #=> UNIXSocket
+#
+class UNIXSocketHack
+  def self.hack(mapping)
+    TCPSocket.singleton_class.class_eval do
+      alias_method :new_without_unixsockhack, :new
+
+      define_method(:new_with_unixsockhack) do |host,service|
+        if val = mapping[[host, service].join(":")]
+          return UNIXSocket.new(val)
+        else
+          return new_without_unixsockhack(host, service)
+        end
+      end
+
+      alias_method :new, :new_with_unixsockhack
+    end
+
+    UNIXSocket.class_eval do
+      alias_method :setsockopt_without_unixsockhack, :setsockopt
+
+      define_method(:setsockopt_with_unixsockhack) do |level, optname, optval|
+        if level == Socket::IPPROTO_TCP
+          # warn "do not setopt by unixsockhack level:#{level}, optname: #{optname}, optval:#{ optval }"
+       else
+         return setsockopt_without_unixsockhack(level, optname, optval)
+       end
+      end
+
+      alias_method :setsockopt, :setsockopt_with_unixsockhack
+    end
+  end
+end
+
--- spec/unix_socket_hack_spec.rb
+++ spec/unix_socket_hack_spec.rb
@@ -1,7 +1,34 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
+require 'tempfile'
 
 describe "UnixSocketHack" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before(:all) do
+    tmp = Tempfile.new('unixsocket')
+    @unix_sock_path = tmp.path + ".sock"
+    UNIXSocketHack.hack({'localhost:9999' => @unix_sock_path })
+
+    @unix_server = UNIXServer.open(@unix_sock_path )
+    @tcp_server  = TCPServer.open('localhost', 9999)
+    @tcp_server2  = TCPServer.open('localhost', 9998)
+  end
+  after(:all) do
+    @unix_server.close
+    @tcp_server.close
+    FileUtils.rm(@unix_sock_path)
+  end
+
+  it "connect to unixsocket:9999 should be UNIXSock" do 
+    sock = TCPSocket.new('localhost', 9999);
+    sock.class.should == UNIXSocket
+  end
+
+  it "connect to localhost:9998 should be TCPSocket" do 
+    sock = TCPSocket.new('localhost', 9998);
+    sock.class.should == TCPSocket
+  end
+
+  it "connect to 127.0.0.1:9999 should be TCPSocket" do 
+    sock = TCPSocket.new('127.0.0.1', 9999);
+    sock.class.should == TCPSocket
   end
 end
